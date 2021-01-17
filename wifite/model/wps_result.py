8de272bc18ae3055a3814bbf4532a640253c9185
MendelGusmao <mendelsongusmao@gmail.com>
2021-01-17 18:14:16 -0300

    Add channel to CrackResultWPS

--- wifite/model/wps_result.py
+++ wifite/model/wps_result.py
@@ -7,9 +7,10 @@ from ..model.result import CrackResult
 import time
 
 class CrackResultWPS(CrackResult):
-    def __init__(self, bssid, essid, pin, psk):
+    def __init__(self, bssid, channel, essid, pin, psk):
         self.result_type = 'WPS'
         self.bssid = bssid
+        self.channel = channel
         self.essid = essid
         self.pin   = pin
         self.psk   = psk
@@ -23,6 +24,7 @@ class CrackResultWPS(CrackResult):
         else:
             psk = '{G}%s{W}' % self.psk
         Color.pl('{+} %s: {C}%s{W}'     % (      'BSSID'.rjust(12), self.bssid))
+        Color.pl('{+} %s: {C}%s{W}'     % (      'Channel'.rjust(12), self.channel))
         Color.pl('{+} %s: {C}WPA{W} ({C}WPS{W})' % 'Encryption'.rjust(12))
         Color.pl('{+} %s: {G}%s{W}'     % (     'WPS PIN'.rjust(12), self.pin))
         Color.pl('{+} %s: {G}%s{W}'     % ('PSK/Password'.rjust(12), psk))
@@ -37,17 +39,18 @@ class CrackResultWPS(CrackResult):
         Color.pl('')
 
     def to_dict(self):
+        print('@@@ to dict', self.__dict__)
         return {
             'type'  : self.result_type,
             'date'  : self.date,
             'essid' : self.essid,
             'bssid' : self.bssid,
+            'channel' : self.channel,
             'pin'   : self.pin,
             'psk'   : self.psk
         }
 
 if __name__ == '__main__':
-    crw = CrackResultWPS('AA:BB:CC:DD:EE:FF', 'Test Router', '01234567', 'the psk')
+    crw = CrackResultWPS('AA:BB:CC:DD:EE:FF', '1', 'Test Router', '01234567', 'the psk')
     crw.dump()
     crw.save()
-
